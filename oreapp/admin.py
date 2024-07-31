75015e3d600fd5032ffedeaace693c3f0f32d3cf
Tiamiyu Abdulmujeeb Olamilekan <abdulmujeebtiamiyu@gmail.com>
2024-07-31 16:40:00 +0100

    admin

--- oreapp/admin.py
+++ oreapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Menu,User,Order
 # Register your models here.
+
+admin.site.register(Menu)
+admin.site.register(Order)
+admin.site.register(User)
