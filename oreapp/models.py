32a873a0264cb68cc2a32785637cb38f5d893f1d
Tiamiyu Abdulmujeeb Olamilekan <abdulmujeebtiamiyu@gmail.com>
2024-08-13 13:01:40 +0100

    display well in the db

--- oreapp/models.py
+++ oreapp/models.py
@@ -21,8 +21,18 @@ class Menu(models.Model):
     is_discounted = models.BooleanField(default=False)
     is_drink = models.BooleanField(default=False)
 
+    def __str__(self):
+        return self.name
+
 
 class Order(models.Model):
     customer = models.ForeignKey(User, on_delete=models.CASCADE)
     menu_items = models.ManyToManyField(Menu)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return (
+            self.name
+            if self.name
+            else f"Order {self.id} - {self.created_at.strftime('%Y-%m-%d %H:%M:%S')}"
+        )
